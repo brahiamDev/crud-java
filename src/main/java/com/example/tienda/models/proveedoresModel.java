f1d10eadb68b6880f1e07c9f4bd8a697c03c5bb0
Brahiam Cortes <brahiamraigoza@gmail.com>
2024-11-05 19:36:20 -0500

    se realiza por completo el crud

---
+++ src/main/java/com/example/tienda/models/proveedoresModel.java
@@ -0,0 +1,23 @@
+package com.example.tienda.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name="proveedores")
+public class proveedoresModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String LastName;
+
+    @Column(name="email", unique = true, nullable = false)
+    private String email;
+}
